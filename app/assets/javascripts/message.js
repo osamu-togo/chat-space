ee9bcc7280d24ead488a9a8c4a75bfaa0ebf6a0b
osamu-togo <ku_ri_ka_e_su@i.softbank.jp>
2019-12-18 17:22:12 +0900

    実装しました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,9 @@
 $(function(){
+
   function buildHTML(message){
   // 「もしメッセージに画像が含まれていたら」という条件式
   if (message.image) {
-    var html = `<p>  <div class="main_chat__message-list__message"> 
+    var html = `<p>  <div class="main_chat__message-list__message" data-message-id="${message.id}"> 
                        <div class="main_chat__message-list__message__info">
                          <div class="main_chat__message-list__message__info__user-name">
                            ${message.user_name}
@@ -18,7 +19,7 @@ $(function(){
                      </div>
                </p>`
   } else {
-    var html = `<P>  <div class="main_chat__message-list__message">
+    var html = `<P>  <div class="main_chat__message-list__message" data-message-id="${message.id}">
                       <div class="main_chat__message-list__message__info">
                         <div class="main_chat__message-list__message__info__user-name">
                           ${message.user_name}
@@ -60,4 +61,32 @@ $(function(){
     alert("メッセージ送信に失敗しました");
   });
 })
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.main_chat__message-list__message').last().data('message-id')
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      //メッセージが入ったHTMLに、入れ物ごと追加
+      $('.main_chat__message-list').append(insertHTML);
+      $('.main_chat__message-list').animate({ scrollTop: $('.main_chat__message-list')[0].scrollHeight});
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  setInterval(reloadMessages, 7000);
 });
